9bd3b78d1fa816edfb8034c7a561e8bc1237aa32
ChenyuWang508 <52644570+wcy111@users.noreply.github.com>
2022-03-31 14:17:34 -0400

    add tech report
    add tech report

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import './App.css';
 import React from 'react';
+import { NavLink, Routes, Route } from 'react-router-dom';
 
 // atob is deprecated but this function converts base64string to text string
 const decodeFileBase64 = (base64String) => {
@@ -14,6 +15,7 @@ const decodeFileBase64 = (base64String) => {
 
 
 function App() {
+
   const [inputFileData, setInputFileData] = React.useState(''); // represented as bytes data (string)
   const [outputFileData, setOutputFileData] = React.useState(''); // represented as readable data (text string)
   const [buttonDisable, setButtonDisable] = React.useState(true);
@@ -69,7 +71,10 @@ function App() {
     backgroundSize: 'cover',
     backgroundRepeat: 'no-repeat',
 };
-
+// const testA={
+//   fontSize:'50px',
+//   color:'red',
+// };
   // handle file submission
   const handleSubmit = (event) => {
     event.preventDefault();
@@ -109,29 +114,39 @@ function App() {
       console.log('POST request success');
     })
   }
-
-  return (
-  
-    <div className="App"  style={myStyle}>
-
-      <h2>Fast style transfer</h2>
+  const App = () => (
+    <div className='app'>
+      <h1>Portfolio Website</h1>
+      <Navigation />
+      <Main />
+     </div>
+  );
+  const Navigation = () => (
+    <nav>
+      <ul>
+        <li><NavLink exact activeClassName="current" to='/'>Home</NavLink></li>
+        <p>"%20%20%20%20%20"</p>
+        <li><NavLink exact activeClassName="current" to='/about'>About</NavLink></li>
+      </ul>
+    </nav>
+  );
+  const Home = () => (
+    <div className='home'>
+      <p> Feel free to browse around and learn more about me.</p>
       <div className="Image">
       <img src="https://picmatix.com/static/index/img/leopard.jpg" height = "250" />
       <img src="https://picmatix.com/static/index/img/catrina-street-art-512.jpg" height = "250" />
       <img src="https://picmatix.com/static/index/img/leopard-street-art.jpg" height = "250" />
-      <p fontSize="1"><strong>1. Input is expected to be a small size file named content.png in advance
+      <p fontSize="1"><strong>
+      1. Input is expected to be a small size file named content.png in advance
       <br></br>
+      <pr></pr>
       2. Output is a style transfomed image file with size 224*224 pixels </strong></p>
       </div>
-      <div className="Input" style={myStyle}>
+      {/* ------Input------- */}
+      <div className="Input" >
         <h2>Input</h2>
         <form onSubmit={handleSubmit}>
-          {/* <label class = "custom-file-upload">
-          <input type="file" accept=".png"   onChange={handleChange} />
-          Custom Upload
-          
-          </label> */}
-
           <label for="file-upload" class="button-6">
          <i class="fa fa-cloud-upload"></i>  Upload Image
           </label>
@@ -140,18 +155,49 @@ function App() {
         <button type="Submit" class="button-6"  disabled={buttonDisable}>{buttonText}</button>
         </div>
         </form>
-        {/* <img src={inputFileData} alt="None"/> */}
-
+      </div>
+      {/* ------Output------- */}
       <div className="Output">
-        <h2>Results</h2>
+        <h2>Result</h2>
         {/* <p>{outputFileData}</p> */}
         
         <img src={outputFileData} alt=""/>
-      </div>
+    
     </div>
     </div>
-
   );
+  const About = () => (
+    <div className='about'>
+      <h1>About Me</h1>
+      <p>Ipsum dolor dolorem consectetur est velit fugiat. Dolorem provident corporis fuga saepe distinctio ipsam? Et quos harum excepturi dolorum molestias?</p>
+      <p>Ipsum dolor dolorem consectetur est velit fugiat. Dolorem provident corporis fuga saepe distinctio ipsam? Et quos harum excepturi dolorum molestias?</p>
+    
+    </div>
+  );
+  const Main = () => (
+    <Routes>
+      <Route exact path='/' element={<Home/>}></Route>
+      <Route exact path='/about' element={<About/>}></Route>
+    </Routes>
+  );
+  return (
+    
+    <div className="App"  style={myStyle}>
+      <br></br>
+      
+      <div className='app'>
+      <h1>Fast style transfer</h1>
+      <Navigation />
+      <Main />
+      </div>
+
+
+    
+
+    
+  </div>
+  );
+  
 }
 
 export default App;
